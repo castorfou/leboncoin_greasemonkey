ad3f37cee418217017d4c7868e943cb632cf2c1c
Guillaume Ramelet <guillaume.ramelet@gmail.com>
2021-05-09 23:42:30 +0200

    store price

--- leboncoin immo.user.js
+++ leboncoin immo.user.js
@@ -34,6 +34,36 @@
         return '';
     }
 
+    function _isContains(json, value) {
+        let contains = false;
+        Object.keys(json).some(key => {
+            contains = typeof json[key] === 'object' ?
+                _isContains(json[key], value) : json[key] === value;
+            return contains;
+        });
+        return contains;
+    }
+
+    function conserve_annonce(id_annonce, auj, prix_annonce) {
+        var annonce_existante = GM_getValue(id_annonce)
+        var fiche_annonce
+        if (annonce_existante == undefined) {
+            //console.log('nouvelle annonce '+id_annonce)
+            fiche_annonce = JSON.parse('{"prix":[{}] }')
+        } else {
+            //console.log('annonce existante '+id_annonce)
+            fiche_annonce = JSON.parse(annonce_existante)
+
+            //pour reinitialiser les valeurs au lancement
+            //à commenter quand c'est bien initialisé
+            //fiche_annonce = JSON.parse('{"prix":[{}] }')
+        }
+        if (! _isContains(fiche_annonce,auj)) {
+            fiche_annonce.prix.push({ date: auj, prix: prix_annonce });
+        }
+        //console.log('contenu fiche '+JSON.stringify(fiche_annonce, null, 2))
+        GM_setValue(id_annonce, JSON.stringify(fiche_annonce, null, 2));
+    }
 
 
 
@@ -48,6 +78,9 @@
     for (var i=0; i<page.length;i++) {
         print_page+=' '+page[i]
     }
+    var today = new Date()
+    var aujourdhui = today.toISOString().split('T')[0];
+    var id_annonce = ''
 
     console.log(page[3])
 
@@ -57,21 +90,31 @@
         prix_annonce=prix_annonce.split('&nbsp;')[0].replace(/\s/g, '');
         console.log('prix affiché : '+prix_annonce)
         var og_url = getMeta('og:url');
-        var id_annonce = og_url.split('/')[4].split('.')[0];
+        id_annonce = og_url.split('/')[4].split('.')[0];
         console.log('annonce id '+id_annonce);
-        var today = new Date()
-        var aujourdhui = today.toISOString().split('T')[0];
         console.log(aujourdhui);
 
         var contenu_cache = GM_getValue(id_annonce)
         console.log('contenu cache '+contenu_cache)
 
-        GM_setValue(id_annonce, aujourdhui+' '+prix_annonce);
-
     }
 
     if (page[3].startsWith('recherche?category=9')) {
         console.log('liste des annonces...')
+        var liste_annonces = document.getElementsByClassName("styles_adCard__2YFTi styles_classified__aKs-b");
+        //console.log(liste_annonces[0]).singleNodeValue
+        //http://xpather.com/  AdCard__AdCardLink-sc-1h74x40-0 cHZrAn
+        for (i=0; i<liste_annonces.length; i++) {
+            var id_annonce_lien = document.evaluate('. //a[@class="AdCard__AdCardLink-sc-1h74x40-0 cHZrAn"]/@href', liste_annonces[i], null, XPathResult.STRING_TYPE, null).stringValue;
+            id_annonce = id_annonce_lien.split('/')[2].split('.')[0]
+            //console.log('ID annonce: '+id_annonce)
+            var prix_annonce_complet = document.evaluate('. //span[@class="AdCardPrice__Amount-bz31y2-1 dflscE"]', liste_annonces[i], null, XPathResult.STRING_TYPE, null).stringValue;
+            prix_annonce = prix_annonce_complet.replace(/(\s*)(€*)/g, '');
+            //console.log(prix_annonce)
+            conserve_annonce(id_annonce, aujourdhui, prix_annonce);
+        }
+
+
     }
     console.log("leboncoin immo stopping...");
 
